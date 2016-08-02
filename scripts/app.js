e7adeb9b494ef8989033a9769c5a491630e8126e
rupanjalitripathi <rupanjalitripathi@gmail.com>
2016-08-02 09:22:49 +0530

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+angular.module("ngClassifieds", ["ngMaterial"])
+.config(function($mdThemingProvider){
+	$mdThemingProvider.theme('default')
+	.primaryPalette('teal')
+	.accentPalette('orange');
+});
